0816fdd0a7f74f579aa376815a7039d26ba1343a
ksmin1 <songheemin@berkeley.edu>
2024-04-09 21:52:11 -0700

    zero-ed IMU

--- include/IMUutilities.h
+++ include/IMUutilities.h
@@ -22,9 +22,46 @@ float readIMU(){
   // float roll = g.gyro.y;
   // float yaw = g.gyro.z;
 
-  // pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / M_PI;
-  //Imu_readings.roll = atan2(ay, az) * 180.0 / M_PI;
   return pitch;
+}
+
+float calculateMean(float arr[], int size) {
+    float sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+    return sum / size;
+}
+
+// Function to calculate the variance of an array
+float calculateVariance(float arr[], int size) {
+    float mean = calculateMean(arr, size);
+    float variance = 0;
+    for (int i = 0; i < size; ++i) {
+        variance += (arr[i] - mean) * (arr[i] - mean);
+    }
+    return variance / size;
+}
+
+const int countersize = 1000;
+float Angles[countersize];
+float Velocities[countersize];
+
+void firstrun() {
+  float dt = 10;
+  float LegVel = 0;
+  //float LegAngle = 0;
+    for (int i = 0; i < countersize; i++) {
+        float time_now = millis();
+        LegVel = readIMU();
+        //LegVel = readIMU()-Leg_Vel_Initial;
+        //LegAngle = fmod(LegAngle + (LegVel*dt)*0.001*180/PI, 360);
+        //Angles[i] = LegAngle;
+        Velocities[i] = LegVel;
+        Serial.print("Leg Velocity = ");
+        Serial.println(LegVel,4);
+        while (millis()-time_now < dt) {}
+    }
 }
 
 float initializeIMU(){
@@ -38,7 +75,7 @@ float initializeIMU(){
   Serial.println("MPU6050 Found!");
 
   //set accelerometer range
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
   
   //set gyroscope range
   mpu.setGyroRange(MPU6050_RANGE_250_DEG);
@@ -50,46 +87,18 @@ float initializeIMU(){
 
   // Zero-ing the sensor:
   // initialize constants
-  float initializetime1 = 1000;
-  float initializetime2 = 5000;
-  float Leg_Vel_Initial = 0;
-  int count = 1;
+  float initializetime1 = 100; //>30ms
+  
+  //trash first few values
   float time_now = millis();
-
-  //trash first second of values
   while (millis()-time_now < initializetime1) {
-        Leg_Vel_Initial = readIMU();
+    float Leg_Vel_Initial = readIMU();
   }
-  //take avg of next 5 seconds of values
-  time_now = millis();
-  while (millis()-time_now < initializetime2) {
-      count = count+1;
-      Leg_Vel_Initial = Leg_Vel_Initial + readIMU();
-  }
-    Leg_Vel_Initial = Leg_Vel_Initial/count;
-    Serial.println("Time Zeroed, count = "+String(count));
-    delay(1000);
-  return Leg_Vel_Initial;
+  //measure bias
+  firstrun();
+  float meanx0 = calculateMean(Velocities, countersize);
+  Serial.println("Time Zeroed");
+  delay(1000);
+  return meanx0;
 }
-
-
-
-// void setup() {
-//     float time_now = millis();
-//     Serial.begin(115200);
-//     while (!Serial) delay(10);
-//     initializeIMU();
-//     while (millis()-time_now < initializetime1) {
-//         Leg_Vel_Initial = readIMU();
-//     }
-//     while (millis()-time_now < initializetime2) {
-//         count = count+1;
-//         Leg_Vel_Initial = Leg_Vel_Initial + readIMU();
-//     }
-//     Leg_Vel_Initial = Leg_Vel_Initial/count;
-//     Serial.println("Time Zeroed, count = "+String(count));
-//     delay(1000);
-// }
-
-
 
