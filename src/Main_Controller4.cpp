7d414f93b4a1ff2ec562e59e792ce2b4fc0daf03
ksmin1 <songheemin@berkeley.edu>
2024-04-15 15:43:46 -0700

    Controller 4 very reliable

--- src/Main_Controller4.cpp
+++ src/Main_Controller4.cpp
@@ -2,6 +2,8 @@
 // kd changing as function of motor angle
 // kp set to zero
 // kd max about 2.5-3 is good
+// As = 2; Al = 3; Ss = 1;
+// Added torque limits at ends (saturates to zero)
 
 #include "Arduino.h"
 #include <MotorUtilities.h>
@@ -18,9 +20,6 @@
 #define POT_K2 16   // A2
 #define POT_C2 15   // A1
 #define POT_D2 14   // A0
-
-//Button
-#define IMU_RESET_BTN 9 // IMU reset button
 
 //////// Variable definition ////////
 
@@ -47,15 +46,6 @@ const float StaticFrictionTorque = 0.25;
 float TorqueAmplitude = 1.0;
 float K2, C2, D2 = 0.1; // torque slope
 
-// initialize button
-int IMU_BTN_STATE = 0;
-unsigned long debounceDelay = 50;
-unsigned long myTime=millis();
-
-void isr() {  // the function to be called when interrupt is triggered
-  IMU_BTN_STATE = 1;
-}
-
 void setup() {
   //starts Serial Com
   Serial.begin(11250);
@@ -72,10 +62,6 @@ void setup() {
   pinMode(POT_K2, INPUT);
   pinMode(POT_C2, INPUT);
   pinMode(POT_D2, INPUT);
-
-  //button for IMU zero
-  pinMode(IMU_RESET_BTN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(IMU_RESET_BTN), isr, RISING);
 
   initializeIMU();
   bias_pitch = calculate_bias();
@@ -118,10 +104,12 @@ void loop() {
   //Serial.print("    K1: "+String(K1)+" C1: "+String(C1)+ " D1:" + String(D1) + " K2: " + String(K2)+" C2: "+String(C2)+" D2: "+String(D2));
 
   // Torque Eq
-  float As = 2; float Al = 1; float Ss = 1; 
+  float As = 2; float Al = 3; float Ss = 1; float L1 = 4.5; float L2 = 2.5;
   float shoulder = As*(1/PI)*atan(degrees(MotorOut.position-origines.shoulder)-10) + As/2;
-  float leg = Al*(1/PI)*atan(degrees(MotorOut.position-origines.leg)+10) - As/2 - 2*Ss + 1;
-  float switching = -4*(1/PI)*atan(HipAngle-20)+1;
+  float leg = Al*(1/PI)*atan(degrees(MotorOut.position-origines.leg)+10) - As/2 - 2*Ss;
+  float switching = -2*Ss*(1/PI)*atan(HipAngle-20)+1;
+  float limit_leg = -L1*(1/PI)*atan(MotorOut.position-origines.leg-180)+L1/2;
+  float limit_shoulder = -L2*(1/PI)*atan(MotorOut.position-origines.shoulder+180)-L2/2;
 
   MotorIn.t_in = shoulder + leg + switching;
   MotorIn.t_in = constrain(MotorIn.t_in, T_MIN, T_MAX);
@@ -135,7 +123,7 @@ void loop() {
   MotorOut = unpack_reply();
   Serial.print("  kd: " + String(MotorIn.kd_in)+"  T_out: "+String(MotorOut.torque));
   Serial.print("  T_in: " + String(MotorIn.t_in));
-  Serial.println("    MEASURING:  IMU_Ang: " + String(HipAngle)+  "  P_out: "+String(degrees(MotorOut.position)) + "  v_out: "+String(MotorOut.velocity));
+  Serial.println("    MEASURING:  IMU_Ang: " + String(HipAngle)+  "  P_out: "+String(degrees(MotorOut.position)) + "  P_l: "+String(degrees(MotorOut.position-origines.leg))+"  P_s: "+String(degrees(MotorOut.position-origines.shoulder))+"  v_out: "+String(MotorOut.velocity));
 
   while (millis()-time_now < dt) {}
 }
